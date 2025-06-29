82569fd173db33cc8c7935314eee349e85db90a7
Shubham Singh <shubham12342019@gmail.com>
2025-06-29 23:44:11 +0530

    Backend Deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Student, Teacher, Invoice, FeeItem } from '../types';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://kidszone-ho5b.onrender.com/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
